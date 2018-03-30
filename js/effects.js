523b310d7e70b059bed50d028917fd9f6ba2e83e
ryan <ryanjchesnut@gmail.com>
2018-03-30 00:18:11 -0400

    footer, social tags, project section
    Added footer, social media,  and projects section

---
+++ js/effects.js
@@ -0,0 +1,32 @@
+	 	$( ".nav-item1" ).click(function() {
+			greyOut();
+			$( this.firstElementChild ).removeClass("disabled"); 
+			$( this.firstElementChild ).addClass("enabled"); 
+			$( this ).effect( "bounce", "slow" );
+			
+	 	});
+	 	$( ".nav-item2" ).click(function() {
+			greyOut(); 
+			$( this.firstElementChild ).removeClass("disabled"); 
+			$( this.firstElementChild ).addClass("enabled"); 
+  			$( this ).effect( "bounce", "slow" );
+	 	});
+	 	$( ".nav-item3").click(function() {
+			greyOut();
+			$( this.firstElementChild ).removeClass("disabled"); 
+			$( this.firstElementChild ).addClass("enabled"); 
+  			$( this ).effect( "bounce", "slow" );
+	 	});
+		$( ".nav-item4").click(function() {
+			greyOut();
+			$( this.firstElementChild ).removeClass("disabled"); 
+			$( this.firstElementChild ).addClass("enabled"); 
+  			$( this ).effect( "bounce", "slow" );
+	 	});
+		  
+		  var greyOut = function(){
+			 for (i=1; i<=4; i++){
+				$("#nav-" +i).removeClass("enabled"); 
+				 $("#nav-" +i).addClass("disabled"); 
+			}
+		  }
